295e5ea4efbf585692240a05e302c399a8c5fb46
Josh Marusek <jm553020@ohio.edu>
2022-03-29 17:36:14 -0400

    build all functions

--- shapes.cc
+++ shapes.cc
@@ -1,2 +1,61 @@
 #include "shapes.h"
 
+using namespace std;
+
+Shape::Shape(double p_thickness, double p_cost_per_cubic_unit) {
+    thickness = p_thickness;
+    cost_per_cubic_unit = p_cost_per_cubic_unit;
+}
+
+void Shape::input() {
+    cout << "Enter the thickness: ";
+    cin >> thickness;
+    cout << "Enter the cost per cubic unit: ";
+    cin >> cost_per_cubic_unit;
+}
+
+double Shape::area() const {
+    return 0;
+}
+
+
+Circle::Circle(double p_radius) {
+    radius = p_radius;
+}
+
+void Circle::input() {
+    Shape::input();
+    cout << "Enter the radius: ";
+    cin >> radius;
+}
+
+
+Rectangle::Rectangle(double p_length, double p_width) {
+    length = p_length;
+    width = p_width;
+}
+
+void Rectangle::input() {
+    Shape::input();
+    cout << "Enter the length: ";
+    cin >> length;
+    cout << "Enter the width: ";
+    cin >> width;
+}
+
+
+Trapezoid::Trapezoid(double p_base1, double p_base2, double p_height) {
+    base1 = p_base1;
+    base2 = p_base2;
+    height = p_height;
+}
+
+void Trapezoid::input() {
+    Shape::input();
+    cout << "Enter the length of base 1: ";
+    cin >> base1;
+    cout << "Enter the length of base 2: ";
+    cin >> base2;
+    cout << "Enter the height: ";
+    cin >> height;
+}
--- shapes.h
+++ shapes.h
@@ -2,6 +2,7 @@
 #define SHAPES_H
 
 #include <iostream>
+#include <cmath>
 
 class Shape {
     public:
@@ -11,15 +12,13 @@ class Shape {
          * @param p_thickness the thickness of the Shape
          * @param p_cost_per_cubic_unit the cost of the Shape per cubic unit
          */
-        Shape(double p_thickness = 1, double p_cost_per_cubic_unit = 1);
+        Shape(double p_thickness = 0, double p_cost_per_cubic_unit = 0);
 
 
         /**
-         * @brief Set the values for the Shape based on input from an istream.
-         * 
-         * @param ins the istream to read from
+         * @brief Set the values for the Shape based on input from cin.
          */
-        void input(std::istream& ins);
+        void input();
 
 
         /**
@@ -28,9 +27,9 @@ class Shape {
         double cost() const {return thickness * cost_per_cubic_unit * area();}
 
         /**
-         * @return the area of the Shape 
+         * @return the area of the Shape
          */
-        double area() const;
+        virtual double area() const;
 
     private:
         double thickness;            // the thickness of the Shape
@@ -43,15 +42,20 @@ class Circle:public Shape {
          * @brief Construct a new Circle object.
          * 
          * @param p_radius the radius of the Circle
-         * @param p_thickness the thickness of the Circle
-         * @param p_cost_per_cubic_unit the cost of the Circle per cubic unit
          */
-        Circle(double p_radius = 0, double p_thickness = 1, double p_cost_per_cubic_unit = 1);
+        Circle(double p_radius = 0);
+
+
+        /**
+         * @brief Set the values for the Shape based on input from cin.
+         */
+        void input();
+
 
         /**
          * @return the area of the Circle 
          */
-        double area() const;
+        double area() const {return M_PI * radius * radius;}
 
     private:
         double radius;  // the radius of the Circle
@@ -64,15 +68,20 @@ class Rectangle:public Shape {
          * 
          * @param p_length the length of the Rectangle
          * @param p_width the width of the Rectangle
-         * @param p_thickness the thickness of the Rectangle
-         * @param p_cost_per_cubic_unit the cost of the Rectangle per cubic unit
          */
-        Rectangle(double p_length = 0, double p_width = 0, double p_thickness = 1, double p_cost_per_cubic_unit = 1);
+        Rectangle(double p_length = 0, double p_width = 0);
+
+
+        /**
+         * @brief Set the values for the Shape based on input from cin.
+         */
+        void input();
+
 
         /**
          * @return the area of the Circle 
          */
-        double area() const;
+        double area() const {return length * width;}
 
     private:
         double length;  // the length of the Rectangle
@@ -87,15 +96,20 @@ class Trapezoid:public Shape {
          * @param p_base1 the length of the first base of the Trapezoid
          * @param p_base2 the length of the second base of the Trapezoid
          * @param p_height the height of the Trapezoid
-         * @param p_thickness the thickness of the Trapezoid
-         * @param p_cost_per_cubic_unit the cost of the Trapezoid per cubic unit
          */
-        Trapezoid(double p_base1 = 0, double p_base2 = 0, double p_height = 0, double p_thickness = 1, double p_cost_per_cubic_unit = 1);
+        Trapezoid(double p_base1 = 0, double p_base2 = 0, double p_height = 0);
+
+
+        /**
+         * @brief Set the values for the Shape based on input from cin.
+         */
+        void input();
+
 
         /**
          * @return the area of the Circle 
          */
-        double area() const;
+        double area() const {return height * (base1 + base2) / 2.0;}
 
     private:
         double base1;   // the length of the first base of the Trapezoid
